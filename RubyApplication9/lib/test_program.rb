838c68f4ea171d64273ccdb6247d5f634ab700fe
Andrey <aborisov@mirantis.com>
2013-07-16 15:57:57 +0400

    new comment

--- RubyApplication9/lib/test_program.rb
+++ RubyApplication9/lib/test_program.rb
@@ -1,5 +1,20 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+## To change this template, choose Tools | Templates
+## and open the template in the editor.
+#2.between?(1,3)
+#words = ["foo", "bar", "baz"]
+#puts (words.[](1))
+#
+#
+#def string_length_interpolater(incoming_string)
+#  "The string you just gave me has a length of "#{incoming_string.length}
+#end
+#
+#puts( "0 The string you just gave me has a length of "#{incoming_string.length}
+#   )
+#  
+#  puts("2 ThiS iS A vErY ComPlEx SenTeNcE".swapcase)
+#  
+#  puts ("I should look into your problem when I get time".gsub('I', 'HU')
+#    )
+#
 
-puts "Hello World"
-#ne comments#
