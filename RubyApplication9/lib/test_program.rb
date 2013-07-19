68a877c8edd12bae13f9490849d5638ae767efcb
Andrey <aborisov@mirantis.com>
2013-07-19 10:49:18 +0400

    new commint 19.07.2013

--- RubyApplication9/lib/test_program.rb
+++ RubyApplication9/lib/test_program.rb
@@ -9,14 +9,14 @@ def string_length_interpolater(incoming_string)
   "The string you just gave me has a length of "#{incoming_string.length}
 end
 
-#puts( "0 The string you just gave me has a length of "#{incoming_string.length}
-#   )
-#  
-#  puts("2 ThiS iS A vErY ComPlEx SenTeNcE".swapcase)
-#  
-#  puts ("I should look into your problem when I get time".gsub('I', 'HU')
-#    )
-#
+puts( "0 The string you just gave me has a length of "#{incoming_string.length}
+   )
+  
+  puts("2 ThiS iS A vErY ComPlEx SenTeNcE".swapcase)
+  
+  puts ("I should look into your problem when I get time".gsub('I', 'HU')
+    )
+
 
 
 #added new commentary:wq
